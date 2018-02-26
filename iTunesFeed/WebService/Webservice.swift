3298c5296a7ef7b1a0da63bb09d414d175fc5ce8
Dominik Reczek <DominikReczek@gmail.com>
2018-02-26 17:12:49 +0100

    typo fix

--- iTunesFeed/WebService/Webservice.swift
+++ iTunesFeed/WebService/Webservice.swift
@@ -2,7 +2,7 @@ import Foundation
 
 final class WebService {
     func load<A>(resources: Resource<A>, completion: @escaping (A?) -> ()) {
-        URLSession.shared.dataTask(with: resources.url) { (data, respnse, error) in
+        URLSession.shared.dataTask(with: resources.url) { (data, response, error) in
             let result = data.flatMap(resources.parse)
 
             DispatchQueue.main.async {
